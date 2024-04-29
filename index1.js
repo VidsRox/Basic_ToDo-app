39f8093ac299147e423b49e4d12ab07f972b01da
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-04-29 11:15:08 +0530

    updated text to free range

--- index1.js
+++ index1.js
@@ -3,7 +3,7 @@ const express = require('express');
 const validate = require('./types') //slightly uglier syntax
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
